ba0be62e8154d2464477e619c2f855c3cdc2a9d0
chirag982 <chirag178g@gmail.com>
2024-09-10 23:22:45 +0530

    Second code to debug....

---
+++ second.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class second {
+    public static void Main(String[] args) {
+        Scanner sc = new scanner(system.in);
+        System.out.println(sc);
+
+        int fact = 1
+
+        int num = sc.nextInt();
+
+        for (int i=1; i<=num, i++) {
+            fact = fact x i;
+        }
+
+        system.Out.printf("Factorial of %d is %f/n", num, fact);
+    }
+}
